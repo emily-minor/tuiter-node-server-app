564b7e2ed5a1e04138fdb06f70009d1aedcfc82b
emilymlynn <minor.e@husky.neu.edu>
2022-11-20 22:02:04 -0500

    5.0

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-import express from 'express'
+import express from 'express';
 import HelloController
-    from "./controllers/hello-controller.js"
+    from "./controllers/hello-controller.js";
 import UserController
-    from "./controllers/users/users-controller.js"
-const app = express()
+    from "./controllers/users/users-controller.js";
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+const app = express();
 app.use(express.json());
-HelloController(app)
-UserController(app)
-app.listen(4000)
+TuitsController(app);
+HelloController(app);
+UserController(app);
+app.listen(4000);
