6d129c58631c1fa22933cad0d3ae279036bc913f
emilymlynn <minor.e@husky.neu.edu>
2022-11-20 21:28:52 -0500

    3.2

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -6,7 +6,15 @@ const UserController = (app) => {
 }
 
 const findUsers = (req, res) => {
+    const type = req.query.type
+    if(type) {
+        const usersOfType = users
+            .filter(u => u.type === type)
+        res.json(usersOfType)
+        return
+    }
     res.json(users)
 }
+
 
 export default UserController
