40013a14a00cec0761a7d41c6beb76d2dfd78482
emilymlynn <minor.e@husky.neu.edu>
2022-11-20 21:29:40 -0500

    3.3 & 3.4

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -2,8 +2,17 @@ import people from './users.js'
 let users = people
 
 const UserController = (app) => {
-    app.get('/api/users', findUsers)
+    app.get('/api/users', findUsers);
+    app.get('/api/users/:uid', findUserById);
 }
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users
+        .find(u => u._id === userId);
+    res.json(user);
+}
+
 
 const findUsers = (req, res) => {
     const type = req.query.type
